e177e76a34b4edd958bdafd42dc2f912f7bc5aa5
Nico Plyley <nico@phoenixtechlab.com>
2023-08-05 13:34:49 -0700

    :sparkles: Created a server to run off database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
-import app from './app';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import app from './app.js';
+import dbConnect from './config/db.js';
 
-app.listen(8080, () => {
-  console.log('Hello World');
-});
+dotenv.config();
+colors.enable();
+
+const PORT = process.env.NODE_PORT;
+
+dbConnect()
+  .then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`.green);
+    });
+
+    process.on('Unhandled Rejection Error', (err) => {
+      console.error(`Unhandled Rejection Error: ${err.message}`);
+      server.close(() => process.exit(1));
+    });
+  })
+  .catch((err) => {
+    console.error(`Error connecting to database: ${err}`.red);
+  });
