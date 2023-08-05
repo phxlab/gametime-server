e379ee4fa8bb0ed6353d6f783fbe21700d391d4f
Nico Plyley <nico@phoenixtechlab.com>
2023-08-05 13:34:31 -0700

    :sparkles: Added middleware and routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import setupMiddleware from './config/middleware.js';
+import setupRoutes from './config/routes.js';
+import setupCustomMiddleware from './config/customMiddleware.js';
 
 const app = express();
+
+setupMiddleware(app);
+setupRoutes(app);
+setupCustomMiddleware(app);
 
 export default app;
