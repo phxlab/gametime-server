112c2c6982ad994aa5b50b2e14f773ecca75acec
Nico Plyley <nico@luxwaymedia.com>
2023-08-11 15:56:00 -0700

    :white_check_mark: Created all test for current routes

--- tests/stores.test.ts
+++ tests/stores.test.ts
@@ -11,3 +11,30 @@ describe('List all stores with', () => {
     expect(res.body.success).toBeTruthy();
   });
 });
+
+describe('List store with', () => {
+  const invalidStores: string[] = [
+      'test',
+      '123',
+      '614f4c2eebd6b9e9d6aa4560'
+  ];
+
+  const validStores: string[] = [
+      'wildcats',
+      '614f4c2eebd6b9e9d6aa4561'
+  ]
+
+  test.each(invalidStores)('Invalid slug or id - 404', async (storeId) => {
+    const res = await request.get(`/stores/${storeId}`);
+
+    expect(res.statusCode).toBe(404);
+    expect(res.body.success).toBeFalsy();
+  });
+
+  test.each(validStores)('Valid slug or id', async (storeId) => {
+      const res = await request.get(`/stores/${storeId}`);
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.success).toBeTruthy();
+  })
+})
