8be2867049541b336f2929a69c16088d648daee8
Nico Plyley <nico@phoenixtechlab.com>
2023-08-21 14:45:04 -0700

    :rotating_light: Fixed linter error

--- src/blueprints/stores/routes.ts
+++ src/blueprints/stores/routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getStoreById, getStores } from './controller';
-import storeOpenMiddleware from "../../middleware/storeOpen";
+import storeOpenMiddleware from '../../middleware/storeOpen';
 
 const stores = express.Router();
 
