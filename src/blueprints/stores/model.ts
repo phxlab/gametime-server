1b64a4114d63cd8f1eeb46622fb711d3747e40d2
Nico Plyley <nico@phoenixtechlab.com>
2023-08-21 14:07:54 -0700

    :sparkles: Removed waves

--- src/blueprints/stores/model.ts
+++ src/blueprints/stores/model.ts
@@ -1,6 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
+import Wave from '../waves/model'
 
-const storeSchema = new mongoose.Schema({
+interface StoreDocument extends Document {
+  name: string;
+  slug: string;
+  waves: {
+    open: string;
+    close: string;
+  }[];
+  color: string;
+  getOpenWaves(): { open: string; close: string }[];
+}
+
+const storeSchema = new Schema<StoreDocument>({
   name: {
     type: String,
     required: [true, 'Store name is required.'],
@@ -18,22 +30,43 @@ const storeSchema = new mongoose.Schema({
       message: 'Store slug must be url-friendly alphanumeric lowercase with dashes.',
     },
   },
-  waves: [{
-    open: {
-      type: Date,
-      required: [true, 'Wave open date is required.'],
-    },
-    close: {
-      type: Date,
-      required: [true, 'Wave close date is required.'],
-    },
-  }],
+  // waves: [
+  //   {
+  //     type: Schema.Types.ObjectId,
+  //     ref: 'Wave',
+  //   },
+  // ],
   color: {
     type: String,
     required: [true, 'Store color is required.'],
   },
 });
 
-const Store = mongoose.model('Store', storeSchema);
+storeSchema.virtual('waves', {
+  ref: 'Wave', // This should match the model name of your Wave schema
+  localField: '_id',
+  foreignField: 'store',
+});
+
+storeSchema.pre('save', async function () {
+
+  if (this.isNew) {
+    console.log(this.waves)
+    const waveData = this.waves.map((wave) => ({
+      store: this._id,
+      open: wave.open,
+      close: wave.close,
+
+    }));
+
+    await Wave.insertMany(waveData);
+  }
+});
+
+storeSchema.methods.getOpenWaves = async function () {
+  return Wave.find({isClosed: false, store: this._id});
+};
+
+const Store = mongoose.model<StoreDocument>('Store', storeSchema);
 
 export default Store;
