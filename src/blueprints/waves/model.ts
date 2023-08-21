143bb01ddb1c3873a2e84e89a5df664eef682223
Nico Plyley <nico@phoenixtechlab.com>
2023-08-21 14:07:23 -0700

    :sparkles: Seperated waves into model

---
+++ src/blueprints/waves/model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema, Types } from 'mongoose';
+
+export interface Wave extends Document {
+    store: Types.ObjectId;
+    open: string;
+    close: string;
+    isClosed: boolean;
+}
+
+const waveSchema = new Schema<Wave>({
+    store: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Store', // Reference to the Store model
+        required: true
+    },
+    open: {
+        type: String,
+        required: true,
+    },
+    close: {
+        type: String,
+        required: true,
+    },
+    isClosed: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const WaveModel = mongoose.model<Wave>('Wave', waveSchema);
+
+export default WaveModel;
