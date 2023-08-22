a8fea6ba1388e5ae4ec8f80814abde24c396d6dd
Nico Plyley <nico@phoenixtechlab.com>
2023-08-21 23:59:40 -0700

    :sparkles: Added categories to routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,8 +1,10 @@
-import stores from '../blueprints/stores/routes';
 import { Express } from 'express';
+import stores from '../blueprints/stores/routes';
+import categories from '../blueprints/categories/routes';
 
 const setupRoutes = (app: Express) => {
   app.use('/stores', stores);
+  app.use('/categories', categories);
 };
 
 export default setupRoutes;
