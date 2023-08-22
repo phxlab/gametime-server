5b07bb5b6764663475b31fb9501be81ad22927d0
Nico Plyley <nico@phoenixtechlab.com>
2023-08-21 23:56:58 -0700

    :sparkles: Added data for items and categories

--- seeder.ts
+++ seeder.ts
@@ -7,17 +7,21 @@ dotenv.config();
 colors.enable();
 
 import Store from './src/blueprints/stores/model';
-// import Item from './src/blueprints/items/model.ts'
-// import Category from './src/blueprints/categories/model.ts';
+import Category from './src/blueprints/categories/model';
+import Item from './src/blueprints/items/model';
 
 const stores = JSON.parse(fs.readFileSync('./__data__/stores.json', 'utf-8'));
-// const items = JSON.parse(fs.readFileSync(`${__dirname}/__data__/items.json`, 'utf-8'));
-// const categories = JSON.parse(fs.readFileSync(`${__dirname}/__data__/categories.json`, 'utf-8'));
+const items = JSON.parse(fs.readFileSync('./__data__/items.json', 'utf-8'));
+const categories = JSON.parse(
+  fs.readFileSync('./__data__/categories.json', 'utf-8'),
+);
 
 export const importData = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
     await Store.create(stores);
+    await Category.create(categories);
+    await Item.create(items);
     console.log('Database seeded'.green);
     await mongoose.connection.close();
   } catch (err) {
