dc9288ea8c518c86fb99e8157708e219ad779438
Nico Plyley <nico@phoenixtechlab.com>
2023-09-15 10:09:41 -0700

    :sparkles: Added org model

---
+++ src/blueprints/orginizations/model.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+interface CompanyDocument {
+  name: string;
+  slug: string;
+}
+
+const companyModel = new Schema<CompanyDocument>({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  slug: {
+    type: String,
+    required: [true, 'Slug is required'],
+    unique: true,
+  },
+});
+
+export default model<CompanyDocument>('Company', companyModel);
