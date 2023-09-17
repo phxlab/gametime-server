0578ed990c00db628d917a2adc30312b90287505
Nico Plyley <nico@phoenixtechlab.com>
2023-09-17 06:40:37 -0700

    :white_check_mark: Added data for org tests

--- tests/config/setup.ts
+++ tests/config/setup.ts
@@ -1,17 +1,24 @@
 import { beforeAll } from 'bun:test';
 import User from '../../src/blueprints/users/model';
 import { closeMongoose, startMongoose } from './db';
+import Org from '../../src/blueprints/organizations/model';
 
 beforeAll(async () => {
   await startMongoose();
 
   await User.deleteMany();
+  await Org.deleteMany();
 
   const user = await User.create({
     name: 'John Doe',
     email: 'jdoe@gmail.com',
     username: 'john',
     password: 'password',
+  });
+
+  const org = await Org.create({
+    name: 'Test Org',
+    slug: 'test',
   });
 
   global.__token = await user.getToken();
