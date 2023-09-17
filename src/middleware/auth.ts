ef641bda1c8bc29d56f71456b268dbcbe15915f7
Nico Plyley <nico@phoenixtechlab.com>
2023-09-16 19:10:26 -0700

    :sparkles: Created auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,38 @@
+import { middleware } from 'hono/factory';
+import * as jose from 'jose';
+import ErrorResponse from '../errors/errorResponse';
+import User from '../blueprints/users/model';
+import { Context } from 'hono';
+
+const protect = middleware(async (c: Context, next) => {
+  let token = c.req.header('Authorization');
+
+  if (!token || !token.startsWith('Bearer')) {
+    throw new ErrorResponse('Not authorized', 401);
+  }
+
+  token = token.split(' ')[1];
+
+  const encoder = new TextEncoder();
+  const JWT_SECRET = encoder.encode(Bun.env.JWT_SECRET);
+
+  let user;
+
+  try {
+    const { payload } = await jose.jwtVerify(token, JWT_SECRET);
+
+    user = User.findById(payload.id).lean();
+
+    c.set('user', payload.id);
+  } catch (error) {
+    throw new ErrorResponse('Not authorized', 401);
+  }
+
+  if (!user) {
+    throw new ErrorResponse('Not authorized', 401);
+  }
+
+  await next();
+});
+
+export default protect;
