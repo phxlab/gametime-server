d07d8a30458946025d7f3863e90543e95755712b
Nico Plyley <nico@phoenixtechlab.com>
2023-09-18 07:42:41 -0700

    :fire: Removed unneeded spreading of errors

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,43 +1,30 @@
 import { Context } from 'hono';
 import mongooseErrorHandler from './mongoose';
+import handleHonoErrors from './hono';
 
-export interface Error {
-  message: string;
-  stack?: any;
-  statusCode?: number;
-}
-
-const errorHandlers = [mongooseErrorHandler];
+const errorHandlers = [mongooseErrorHandler, handleHonoErrors];
 
 const errorHandler = async (error: any, c: Context) => {
-  let err: Error | null = {
-    message: error.message,
-    stack: error.stack,
-    statusCode: error.statusCode,
-  };
-
   if (error.stack) {
     console.error(error.stack);
   }
 
-  for (const handleError of errorHandlers) {
-    err = handleError(error, err);
+  if (!error.message || !error.statusCode) {
+    for (const handleError of errorHandlers) {
+      error = handleError(error);
 
-    if (err.statusCode) {
-      break;
+      if (error.statusCode) {
+        break;
+      }
     }
-  }
-
-  if (err.message && !err.statusCode) {
-    err.statusCode = 400;
   }
 
   return c.json(
     {
       success: false,
-      message: err.message || 'Internal server error',
+      message: error.message || 'Internal server error',
     },
-    err.statusCode || 500,
+    error.statusCode || 500,
   );
 };
 
