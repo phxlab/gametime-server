7bbd7e050a64573ec9663663ab22e778ce9f9305
Nico Plyley <nico@phoenixtechlab.com>
2023-09-18 08:07:20 -0700

    :sparkles: Created wrapper function to allow parameters

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,31 +1,33 @@
-import { Context } from 'hono';
+import { ErrorHandler } from 'hono';
 import mongooseErrorHandler from './mongoose';
 import handleHonoErrors from './hono';
 
 const errorHandlers = [mongooseErrorHandler, handleHonoErrors];
 
-const errorHandler = async (error: any, c: Context) => {
-  if (error.stack) {
-    console.error(error.stack);
-  }
+const errorHandler =
+  (stack = true): ErrorHandler =>
+  async (error: any, c) => {
+    if (stack && error.stack) {
+      console.error(error.stack);
+    }
 
-  if (!error.message || !error.statusCode) {
-    for (const handleError of errorHandlers) {
-      error = handleError(error);
+    if (!error.message || !error.statusCode) {
+      for (const handleError of errorHandlers) {
+        error = handleError(error);
 
-      if (error.statusCode) {
-        break;
+        if (error.statusCode) {
+          break;
+        }
       }
     }
-  }
 
-  return c.json(
-    {
-      success: false,
-      message: error.message || 'Internal server error',
-    },
-    error.statusCode || 500,
-  );
-};
+    return c.json(
+      {
+        success: false,
+        message: error.message || 'Internal server error',
+      },
+      error?.statusCode || 500,
+    );
+  };
 
 export default errorHandler;
