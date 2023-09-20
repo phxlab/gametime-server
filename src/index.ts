d3b371da9ce913f1108118720be7ef93b61b5f66
Nico Plyley <nico@phoenixtechlab.com>
2023-09-20 14:42:19 -0700

    :sparkles: Add stores

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
+import { errorHandler } from 'hono-error-handler';
 import dbConnect from './config/db';
 import users from './blueprints/users/routes';
 import org from './blueprints/organizations/routes';
 import auth from './blueprints/auth/routes';
 import protect from './middleware/auth';
-import { errorHandler } from 'hono-error-handler';
 import errorHandlers from './errors';
+import stores from './blueprints/stores/routes';
 
 (async () => {
   await dbConnect();
@@ -26,6 +27,7 @@ app.route('/auth', auth);
 app.use('/users/*', protect);
 app.route('/users', users);
 app.route('/org', org);
+app.route('/org/:orgSlug/stores', stores);
 app.onError(errorHandler(errorHandlers));
 
 export default app;
