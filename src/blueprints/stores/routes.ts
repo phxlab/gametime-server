779dfd7eb333388a30bb9f4cd7242df21acb373d
Nico Plyley <nico@phoenixtechlab.com>
2023-09-20 14:40:18 -0700

    :sparkles: Created stores routes

---
+++ src/blueprints/stores/routes.ts
@@ -0,0 +1,70 @@
+import { Context, Hono } from 'hono';
+import { ErrorResponse } from 'hono-error-handler';
+import Store from './model';
+import Org from '../organizations/model';
+import protect from '../../middleware/auth';
+
+const stores = new Hono();
+
+// @desc    Create store
+// *route   POST /org/:orgSlug/stores
+// !method  Private
+stores.post('/', protect, async (c: Context) => {
+  const orgSlug = c.req.param('orgSlug');
+  const org = await Org.findOne({ slug: orgSlug });
+
+  if (!org) {
+    throw new ErrorResponse('Organization does not exist', 404);
+  }
+
+  const { name, slug, color } = await c.req.json();
+
+  const store = await Store.create({
+    name,
+    slug: slug?.toLowerCase(),
+    color,
+    org,
+  });
+
+  return c.json({
+    success: true,
+    data: store,
+  });
+});
+
+// @desc    Get all stores
+// *route   GET /org/:orgSlug/stores
+// ?method  Public
+stores.get('/', async (c: Context) => {
+  const orgSlug = c.req.param('orgSlug');
+
+  const stores = await Org.findOne({ slug: orgSlug }).populate({
+    path: 'stores',
+    select: 'name slug color -org -_id',
+  });
+
+  return c.json({
+    success: true,
+    data: stores,
+  });
+});
+
+// @desc    Get single store
+// *route   GET /org/:orgSlug/stores/:storeSlug
+// ?method  Public
+stores.get('/:storeSlug', async (c: Context) => {
+  const orgSlug = c.req.param('orgSlug');
+  const storeSlug = c.req.param('storeSlug');
+
+  const store = await Org.findOne({ slug: orgSlug }).populate({
+    path: 'store',
+    select: 'name slug color -org -_id',
+  });
+
+  return c.json({
+    success: true,
+    data: store,
+  });
+});
+
+export default stores;
