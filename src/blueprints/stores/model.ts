0b4f4c090d951620f4417d67b01768a11ecc61ee
Nico Plyley <nico@phoenixtechlab.com>
2023-09-20 14:40:10 -0700

    :sparkles: Created stores model

---
+++ src/blueprints/stores/model.ts
@@ -0,0 +1,56 @@
+import { Document, Schema, model } from 'mongoose';
+import { ErrorResponse } from 'hono-error-handler';
+
+interface StoreDocument extends Document {
+  name: string;
+  slug: string;
+  waves: {
+    open: string;
+    close: string;
+  }[];
+  color: string;
+  org: Schema.Types.ObjectId;
+}
+
+const Store = new Schema<StoreDocument>({
+  name: {
+    type: String,
+    required: [true, 'Name is required.'],
+  },
+  slug: {
+    unique: false,
+    type: String,
+    required: [true, 'Slug is required.'],
+    minlength: 4,
+    maxlength: 25,
+    match: [
+      /^[a-zA-Z][a-zA-Z0-9]*$/,
+      'Slug must be alphanumeric and cannot start with a number',
+    ],
+  },
+  color: {
+    type: String,
+    required: [true, 'Color is required.'],
+  },
+  org: {
+    type: Schema.Types.ObjectId,
+    ref: 'Org',
+    required: [true, 'Organization is required.'],
+  },
+});
+
+Store.pre('save', async function (next) {
+  // @ts-ignore
+  const existingStore = await this.constructor.findOne({
+    slug: this.slug,
+    org: this.org,
+  });
+
+  if (existingStore) {
+    next(new ErrorResponse('slug is already registered', 409));
+  }
+
+  next();
+});
+
+export default model('Store', Store);
