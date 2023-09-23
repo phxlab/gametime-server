0552d4d1fcd758bf9195f0192ef553ba98ce4878
Nico Plyley <nico@phoenixtechlab.com>
2023-09-23 15:34:23 -0700

    :sparkles: Exported wave routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,3 +2,4 @@ export { default as auth } from './auth';
 export { default as orgs } from './orgs';
 export { default as stores } from './stores';
 export { default as users } from './users';
+export { default as waves } from './waves';
