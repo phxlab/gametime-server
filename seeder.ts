6279f9363d74ce4d895999c4567882229a8d5ccb
Nico Plyley <nico@phoenixtechlab.com>
2023-09-26 14:49:10 -0700

    :construction: Created seeder

---
+++ seeder.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { Org, Store, Wave } from './src/models';
+
+let MONGO_URI = Bun.env.MONGO_URI as string;
+mongoose
+  .connect(`${MONGO_URI}gametime`)
+  .then(() => console.log('DB Connected'));
+
+const importData = async () => {};
+
+const deleteData = async () => {
+  try {
+    await Org.deleteMany();
+    await Store.deleteMany();
+    await Wave.deleteMany();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+if (process.argv[2] === '-d') {
+  deleteData().then(() => {
+    console.log('Data deleted');
+    process.exit();
+  });
+}
