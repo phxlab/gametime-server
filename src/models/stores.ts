6e4b7984df4d9c60a44a760ee0923846ee81870f
Nico Plyley <nico@phoenixtechlab.com>
2023-09-27 16:47:16 -0700

    :sparkles: Added virtual

--- src/models/stores.ts
+++ src/models/stores.ts
@@ -1,5 +1,6 @@
 import { Document, model, Schema } from 'mongoose';
 import { ErrorResponse } from 'hono-error-handler';
+import { WaveDocument } from './waves';
 
 export interface StoreDocument extends Document {
   name: string;
@@ -7,38 +8,45 @@ export interface StoreDocument extends Document {
   color: string;
   archived: boolean;
   org: Schema.Types.ObjectId;
+  wave?: WaveDocument;
 }
 
-const Store = new Schema<StoreDocument>({
-  name: {
-    type: String,
-    required: [true, 'Name is required.'],
+const Store = new Schema<StoreDocument>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required.'],
+    },
+    slug: {
+      unique: false,
+      type: String,
+      required: [true, 'Slug is required.'],
+      minlength: 4,
+      maxlength: 25,
+      match: [
+        /^[a-zA-Z][a-zA-Z0-9]*$/,
+        'Slug must be alphanumeric and cannot start with a number',
+      ],
+    },
+    color: {
+      type: String,
+      required: [true, 'Color is required.'],
+    },
+    archived: {
+      type: Boolean,
+      default: false,
+    },
+    org: {
+      type: Schema.Types.ObjectId,
+      ref: 'Org',
+      required: [true, 'Organization is required.'],
+    },
   },
-  slug: {
-    unique: false,
-    type: String,
-    required: [true, 'Slug is required.'],
-    minlength: 4,
-    maxlength: 25,
-    match: [
-      /^[a-zA-Z][a-zA-Z0-9]*$/,
-      'Slug must be alphanumeric and cannot start with a number',
-    ],
+  {
+    toJSON: { virtuals: true, versionKey: false },
+    toObject: { virtuals: true },
   },
-  color: {
-    type: String,
-    required: [true, 'Color is required.'],
-  },
-  archived: {
-    type: Boolean,
-    default: false,
-  },
-  org: {
-    type: Schema.Types.ObjectId,
-    ref: 'Org',
-    required: [true, 'Organization is required.'],
-  },
-});
+);
 
 Store.pre('save', async function (next) {
   // @ts-ignore
@@ -52,6 +60,13 @@ Store.pre('save', async function (next) {
   }
 
   next();
+});
+
+Store.virtual('wave', {
+  ref: 'Wave',
+  localField: '_id',
+  foreignField: 'store',
+  justOne: true,
 });
 
 export default model('Store', Store);
