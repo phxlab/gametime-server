dca5733dee68b932e76d891735dd73ffa0cbbb23
Nico Plyley <nico@phoenixtechlab.com>
2023-10-01 15:47:05 -0700

    :sparkles: Exported new models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,5 @@
+export { default as Category } from './categories';
+export { default as Item } from './items';
 export { default as Org } from './orgs';
 export { default as Store } from './stores';
 export { default as User } from './users';
