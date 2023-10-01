175997c1db5d8f4acd5018c51839b9a9115a94ae
Nico Plyley <nico@phoenixtechlab.com>
2023-10-01 16:28:22 -0700

    :white_check_mark: Added todo tests

---
+++ tests/categories.test.ts
@@ -0,0 +1,30 @@
+import { describe, test } from 'bun:test';
+
+describe('Create category', () => {
+  test.todo('with no auth - 401');
+  test.todo('with no data - 400');
+  test.todo('success - 201');
+});
+
+describe('Get all categories', () => {
+  test.todo('with no auth - 200');
+  test.todo('success - 200');
+});
+
+describe('Get category', () => {
+  test.todo('with invalid id - 404');
+  test.todo('with no auth - 200');
+  test.todo('success - 200');
+});
+
+describe('Update category', () => {
+  test.todo('with no auth - 401');
+  test.todo('with no data - 400');
+  test.todo('success - 200');
+});
+
+describe('Delete category', () => {
+  test.todo('with no auth - 401');
+  test.todo('with no data - 400');
+  test.todo('success - 200');
+});
